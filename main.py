300b1a2eace89f9678ec22d2288d18fc783c534c
zezimyy <66038323+zezimyy@users.noreply.github.com>
2023-05-17 00:32:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+dia_inicial = 1
+dias_finais = int(input("Por quantos dias deseja lucrar 1%?: "))
+montante = float(input("Qual o montante?: "))
+
+print("Juros:")
+while dia_inicial <= dias_finais:
+  print("{:.2f} - {}".format(montante, dia_inicial))
+  dia_inicial += 1
+  montante = montante * 1.01
+
+print("Ao final voce tera um total de: {:.2f} reais".format(montante))
